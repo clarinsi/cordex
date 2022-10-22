51004ee897d730773a0eca8b355f6f6c50d8074a
Luka <krsnik.luka92@gmail.com>
2022-10-22 15:36:05 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -7,4 +7,13 @@ setup(name='cordex',
   author_email='fake@mail.com',
   license='MIT',
   packages=find_packages(),
+  install_requires=[
+    'conllu==4.5.2',
+    'conversion-utils @ git+https://gitea.cjvt.si/generic/conversion_utils.git@2f74dfcab890f50e52426018e350faabfac11742',
+    'importlib-resources==5.4.0',
+    'lxml==4.9.1',
+    'tqdm==4.64.1',
+    'zipp==3.6.0',
+    'requests==2.21.0'
+  ],
 )
