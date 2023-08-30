6eb5181ac939ee15793a46b64e210d73af019249
Cyprian Laskowski <cyp@cjvt.si>
2023-08-30 18:42:48 +0200

    Updated dependency versions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 here = path.abspath(path.dirname(__file__))
 
-# read the version from classla/_version.py
+# read the version from cordex/_version.py
 version_file_contents = open(path.join(here, 'cordex/_version.py'), encoding='utf-8').read()
 VERSION = re.compile('__cordex_version__ = \'(.*)\'').search(version_file_contents).group(1)
 
@@ -18,10 +18,10 @@ setup(name='cordex',
   packages=find_packages(),
   install_requires=[
     'conllu==4.5.2',
-    'conversion-utils @ git+https://gitea.cjvt.si/generic/conversion_utils@89bcde58aa3ca01462808af74a9f39f488b1bbd0',
+    'conversion-utils @ git+https://gitea.cjvt.si/generic/conversion_utils@89be60310380339f97ccd2feee65d5d69859e7f9',
     'importlib-resources==5.4.0',
     'lxml==4.9.1',
-    'tqdm>=4.64.1',
+    'tqdm>=4.62.3',
     'zipp==3.6.0',
     'requests>=2.21.0'
   ],
