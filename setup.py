7087727d726560a9efa8262aae7d4d37642c91f7
Luka <krsnik.luka92@gmail.com>
2023-08-30 13:29:32 +0200

    Updated setup and requirements.txt

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ setup(name='cordex',
     'conversion-utils @ git+https://gitea.cjvt.si/generic/conversion_utils@89bcde58aa3ca01462808af74a9f39f488b1bbd0',
     'importlib-resources==5.4.0',
     'lxml==4.9.1',
-    'tqdm==4.64.1',
+    'tqdm>=4.64.1',
     'zipp==3.6.0',
-    'requests==2.21.0'
+    'requests>=2.21.0'
   ],
 )
