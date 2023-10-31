14508619dca12db2d3c3610ccd55b1874e7a40ba
lkrsnik <krsnik.luka92@gmail.com>
2023-10-31 10:57:42 +0100

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ from setuptools import setup, find_packages
 
 here = path.abspath(path.dirname(__file__))
 
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+  long_description = f.read()
+
 # read the version from classla/_version.py
 version_file_contents = open(path.join(here, 'cordex/_version.py'), encoding='utf-8').read()
 VERSION = re.compile('__cordex_version__ = \'(.*)\'').search(version_file_contents).group(1)
@@ -12,13 +15,16 @@ VERSION = re.compile('__cordex_version__ = \'(.*)\'').search(version_file_conten
 setup(name='cordex',
   version=VERSION,
   description=u"Parser for collocability",
-  author='CLARIN.SI',
-  author_email='info@clarin.si',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url='https://github.com/clarinsi/cordex',
+  author='CJVT',
+  author_email='pypi@cjvt.si',
   license='MIT',
   packages=find_packages(),
   install_requires=[
     'conllu>=4.5.2',
-    'conversion-utils @ git+https://gitea.cjvt.si/generic/conversion_utils@89be60310380339f97ccd2feee65d5d69859e7f9',
+    'cjvt-conversion-utils>=0.3',
     'importlib-resources>=5.4.0',
     'lxml>=4.9.1',
     'tqdm>=4.62.3',
